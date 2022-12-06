50ac8c4e741a331bcd4066d679529b02f6d338bc
wbach <91107968+Will-Bach90@users.noreply.github.com>
2022-12-06 14:06:49 -0800

    Update BankAccount.cpp

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -1,6 +1,12 @@
 #include "BankAccount.h"
 #include <iostream>
 
+// const string FUND_TYPES[10] = {"Money Market","Prime Money Market","Long-Term Bond","Short-Term Bond",
+// 								"500 Index Fund","Capital Value Fund","Growth Equity Fund","Growth Index Fund",
+// 								"Value Fund","Value Stock Index"};
+
+// ============== Constructor/Destructors =============
+
 BankAccount::BankAccount() : last(""), first(""), id(0) {}
 
 BankAccount::BankAccount(string last, string first, int id)
@@ -8,9 +14,17 @@ BankAccount::BankAccount(string last, string first, int id)
 	this->lastname = last;
 	this->firstname = first;
 	this->accountID = id;
+	for (int i = 0; i < 10; i < i++)
+	{
+		funds[i].setFundName(FUND_TYPES[i]);
+		funds[i].setFundBalance(0);
+	}
 }
 
 BankAccount::~BankAccount(){}
+
+// ============ Accessor Functions =================
+// ============ Getter/Setters =====================
 
 int BankAccount::getLastName() const
 {
@@ -27,17 +41,31 @@ int BankAccount::getLastName() const
 	return accountID;
 }
 
-void BankAccount::setLastName(string last)
+void BankAccount::setLastName(string lname)
 {
-	this->lastname = last;
+	this->lastname = lname;
 }
 
-void BankAccount::setFirstName(string first)
+void BankAccount::setFirstName(string fname)
 {
-	this->firstname = first;
+	this->firstname = lname;
 }
 
-void BankAccount::setID(string newID)
+void BankAccount::setID(string id)
 {
-	this->accountID = newID;
+	this->accountID = id;
 }
+
+// =============== Print BankAccount History ==============
+
+void BankAccount::printAccountHistory()
+{
+
+	cout << "Transaction History for " << this->firstname << this->lastname << " by fund.\n"
+	for(int i = 0; i < 10; i++)
+	{
+		funds[i].printFundHistory();
+	}
+}
+
+// =============== Operation Overloading ===================
