c7b1130f32f17aeb449164c94bf81dbad60f5953
Min0322 <119825544+Min0322@users.noreply.github.com>
2022-12-07 16:20:53 -0800

    Create Funds.h

---
+++ Funds.h
@@ -0,0 +1,36 @@
+// Funds.h
+// Minseong Na
+// Created on 12-07-22
+// A prat of Program 5 Jolly Banker
+
+#progma once
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Funds
+{
+    public:
+        int getFundType();
+        int getBalance();
+        void setFundType(string FundType);
+        void setBalance(int amt);
+
+        addMoney(int amt);
+        subtractMoney(int amt);
+        addToHistory(string txn);
+        addToHistoryWithError(string txn);
+        printFundHistory();
+        bool withdrawPossible(int amt);
+
+    private:
+
+        string fundType;
+        int balance;
+        string history[];
+
+
+}
