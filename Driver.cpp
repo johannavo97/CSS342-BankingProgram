901a35622f3f0ed8bb70f0d3b503726fa7fd2e78
wbach <91107968+Will-Bach90@users.noreply.github.com>
2022-12-11 02:16:56 -0800

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -1,2 +1,14 @@
 //Driver.cpp
 //Group Random Driver Code
+#include "Banker.h"
+
+
+int main()
+{
+    Banker b;
+    b.readTransactionsFromFile("BankTransIn.txt");
+    b.processTransactions();
+    b.printOutBalances();
+
+    return 0;
+}
