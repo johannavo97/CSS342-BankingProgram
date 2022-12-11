aefd03e5d2cb7b5e8d3b7bdf0c4233ee6c21131f
Chris Jensen <119817689+ChrisJen02@users.noreply.github.com>
2022-12-11 11:22:19 -0800

    Update Transaction.cpp
    add history to funds

--- Transaction.cpp
+++ Transaction.cpp
@@ -1,31 +1,24 @@
 // Transactions.cpp
 // Created by Chris Jensen
 // Created on December 9, 2022
-// Last Edited December 10, 2022s
+// Last Edited December 11, 2022
 // A part of Program5 JollyBanker
 
 #include "Transactions.h"
-#include "BankAccount.h"
-
-
-
-
 	//Gathers account information from banker and creates account instance which is inserted into BSTree
 bool Transactions::openAccount(BSTree::Node* headNode, const string lastName, const string firstName, const int accountId)
 {
 	BSTree accountBST = new BSTree;
 	accountBST.Insert(headNode);
 	BankAccount* current = nullptr;
-	if (accountBST.Retrieve(accountID, current)
+	if (accountBST.Retrieve(accountId, current))
 	{
 		//error message should be written to outfile account is already open
 		return false;
 	}
-	else {
-		BankAccount* newAccount = new BankAccount(lastName, firstName, accountId);
-			accountBST.Insert(newAccount);
-			return true;
-	}
+	BankAccount newAccount = new BankAccount(lastName, firstName, accountId);
+	accountBST.Insert(newAccount);
+	return true;
 }
 
 
@@ -33,6 +26,8 @@ bool Transactions::openAccount(BSTree::Node* headNode, const string lastName, co
 bool Transactions::deposit(BankAccount* account, int fundType, int dollarAmount)
 {
 	account->funds[fundType].addMoney(dollarAmount);
+	string transaction = "D " + account->getID() + fundType + " " + dollarAmount;
+	addToHistory(account, fundType, transaction);
 	return true;
 }
 
@@ -40,6 +35,7 @@ bool Transactions::deposit(BankAccount* account, int fundType, int dollarAmount)
 //Withdraws funds from given fund type from an account already retrieved from Banker.coo
 bool Transactions::withdraw(BankAccount* account, int fundType, int dollarAmount)
 {
+	string transaction = "W " + account->getID + fundType + " " + dollarAmount;
 	// handles fund 0-3 for linked types
 	if (fundType >= 0 && fundType <= 3) {
 		// only enters if fund is between 0 and 3
@@ -47,66 +43,82 @@ bool Transactions::withdraw(BankAccount* account, int fundType, int dollarAmount
 		if (fundType == 0) {
 			if (dollarAmount <= account->getSubAccBalance(fundType)) {
 				account->funds[fundType].subtractMoney(dollarAmount);
+				addToHistory(account, fundType, transaction);
 				return true;
 			}
 			if (dollarAmount <= (account->getSubAccBalance(fundType) + account->getSubAccBalance(1))) {
 				int remainder = dollarAmount - account->getSubAccBalance(fundType);
 				account->funds[fundType].subtractMoney(account->getSubAccBalance(fundType));
 				account->funds[1].subtractMoney(remainder);
+				addToHistory(account, fundType, transaction);
 				return true;
 			}
+			transaction = transaction + " (failed)";
+			addToHistory(account, fundType, transaction);
 			return false;
 		}
 		// Fund 1 linked with fund 0
 		if (fundType == 1) {
 			if (dollarAmount <= account->getSubAccBalance(fundType)) {
 				account->funds[fundType].subtractMoney(dollarAmount);
+				addToHistory(account, fundType, transaction);
 				return true;
 			}
 			if (dollarAmount <= account->getSubAccBalance(fundType) + account->getSubAccBalance(0)) {
 				int remainder = dollarAmount - account->getSubAccBalance(fundType);
 				account->funds[fundType].subtractMoney(account->getSubAccBalance(fundType));
 				account->funds[0].subtractMoney(remainder);
+				addToHistory(account, fundType, transaction);
 				return true;
 			}
+			transaction = transaction + " (failed)";
+			addToHistory(account, fundType, transaction);
 			return false;
 		}
 		// Fund 2 linked with fund 3
 		if (fundType == 2) {
 			if (dollarAmount <= account->getSubAccBalance(fundType)) {
 				account->funds[fundType].subtractMoney(dollarAmount);
+				addToHistory(account, fundType, transaction);
 				return true;
 			}
 			if (dollarAmount <= account->getSubAccBalance(fundType) + account->getSubAccBalance(3)) {
 				int remainder = dollarAmount - account->getSubAccBalance(fundType);
 				account->funds[fundType].subtractMoney(account->getSubAccBalance(fundType));
 				account->funds[3].subtractMoney(remainder);
+				addToHistory(account, fundType, transaction);
 				return true;
 			}
+			transaction = transaction + " (failed)";
+			addToHistory(account, fundType, transaction);
 			return false;
 		}
 		// Fund 3 linked with fund 2
 		if (fundType == 3) {
 			if (dollarAmount <= account->getSubAccBalance(fundType)) {
 				account->funds[fundType].subtractMoney(dollarAmount);
+				addToHistory(account, fundType, transaction);
 				return true;
 			}
 			if (dollarAmount <= account->getSubAccBalance(fundType) + account->getSubAccBalance(2)) {
 				int remainder = dollarAmount - account->getSubAccBalance(fundType);
 				account->funds[fundType].subtractMoney(account->getSubAccBalance(fundType));
 				account->funds[2].subtractMoney(remainder);
+				addToHistory(account, fundType, transaction);
 				return true;
 			}
+			transaction = transaction + " (failed)";
+			addToHistory(account, fundType, transaction);
 			return false;
 		}
 	}
 
 	// funds 4-9
-	if (dollarAmount > account->getSubAccBalance(fundType)) {	
+	if (dollarAmount > account->getSubAccBalance(fundType)) {
 		return false;
 	}
-
 	account->funds[fundType].subtractMoney(dollarAmount);
+	addToHistory(account, fundType, transaction);
 	return true;
 }
 
@@ -116,6 +128,8 @@ bool Transactions::transfer(BankAccount* accountOne, int accountOneFund, BankAcc
 {
 	if (withdraw(accountOne, accountOneFund, dollarAmount)) {
 		deposit(accountTwo, accountTwoFund, dollarAmount);
+		string transaction = "T " + accountOne->getID() + accountOneFund + " " + dollarAmount;
+		addToHistory(account, fundType, transaction);
 		return true;
 	}
 
@@ -123,9 +137,9 @@ bool Transactions::transfer(BankAccount* accountOne, int accountOneFund, BankAcc
 }
 
 
-
 //Adds the transaction string to indidivdual funds 
 bool Transactions::addToHistory(BankAccount* accountId, int fundType, string transaction)
 {
-	return false;
+	accountId->funds[fundType].addToHistory(transaction);
+	return true;
 }
